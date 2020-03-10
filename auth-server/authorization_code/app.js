a63eebafa3bbb99ea34bae00f3b549c4c26362c9
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2020-03-10 17:45:44 -0400

    added dotenv

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -2,10 +2,11 @@ var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
-
-var { client_id, client_secret } = require('../../secrets');
+require('dotenv').config();
 
 var redirect_uri = 'http://localhost:8888/callback'; 
+var client_id = process.env.API_SPOTIFY_CLIENT_ID;
+var client_secret = process.env.API_SPOTIFY_CLIENT_SECRET;
 
 /**
  * Generates a random string containing numbers and letters
