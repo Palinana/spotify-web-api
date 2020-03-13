696fb463bb123e593c4b608eb5581048d512d42e
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2020-03-12 21:40:09 -0400

    removed one app.use

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -33,11 +33,6 @@ var app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')))
   .use(cookieParser());
 
-// sends index.html
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-})
-
 app.get('/login', function(req, res) {
 
   var state = generateRandomString(16);
