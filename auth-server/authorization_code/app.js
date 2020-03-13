12fcc3787c04da9334b3eb80fbcdeb65d2dcdfcd
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2020-03-13 17:55:05 -0400

    updated build

--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -29,9 +29,14 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
-// app.use(express.static(path.join(__dirname, '..', 'public')))
-app.use(express.static(__dirname, '..', 'public'))
+// app.use(express.static(__dirname, '..', 'public'))
+app.use(express.static(path.join(__dirname, '..', 'public')))
   .use(cookieParser());
+
+// sends index.html
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
 
 app.get('/login', function(req, res) {
 
