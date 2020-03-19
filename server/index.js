33b530f4d6bb15d1b6516d840257743cc561c290
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2020-03-19 17:01:47 -0400

    serving client html

--- server/index.js
+++ server/index.js
@@ -40,9 +40,9 @@ if (process.env.NODE_ENV === 'production') {
 
     app.use(express.static(path.join(__dirname, '../public')));
 
-    // app.use('*', (req, res, next) => {
-    //   res.sendFile(path.join(__dirname, '../client/public/index.html'));
-    // });
+    app.use('*', (req, res, next) => {
+      res.sendFile(path.join(__dirname, '../client/public/index.html'));
+    });
   
     app.use((err, req, res, next) => {
       console.error(err);
