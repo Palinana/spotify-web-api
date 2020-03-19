890a8032bbb6318282f1f153ac8d798dd48a7f94
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2020-03-19 17:34:55 -0400

    removed concurrently

--- server/index.js
+++ server/index.js
@@ -33,6 +33,7 @@ var app = express();
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+    app.use(express.static('server/public/index.html'));
     
 } else {
     app.use(express.static(__dirname + '/public'))
